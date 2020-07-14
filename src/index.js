dda34320b3e1b09d55a8b9e9c7d6d86b2027e525
Yevgen <yevgen.code@gmail.com>
2020-07-14 13:14:35 +0300

    add action and store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+import {Provider} from 'react-redux';
+import {BrowserRouter as Router} from 'react-router-dom';
+import ErrorBoundry from './components/error-boundry';
+import RestoService from './services/resto-service';
+import RestoServiceContext from './components/resto-service-context';
+import store from './store';
 
 import './index.scss';
 
