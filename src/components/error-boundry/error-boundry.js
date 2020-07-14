d0a7f2e62eb19059be9cd007c906d3483e754cfb
Yevgen <yevgen.code@gmail.com>
2020-07-14 12:25:04 +0300

    обработали границы выполнения ошибокЭ

--- src/components/error-boundry/error-boundry.js
+++ src/components/error-boundry/error-boundry.js
@@ -2,8 +2,20 @@ import React, {Component} from 'react';
 import Error from '../error';
 
 export default class ErrorBoundry extends Component {
+
+    state = {
+        error: false
+    }
+
+    componentDidCatch() {
+        this.setState({error: true})
+    }
+
     render() {
+       if (this.state.error) {
+        return <Error/>
+       }
        
-        return 1;
+       return this.props.children;
     }
 }
